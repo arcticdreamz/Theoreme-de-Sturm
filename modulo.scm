de33efb0fd0d64acedeacf9ecb335063937df4a7
arcticdreamz <tomcrasset@gmail.com>
2018-04-08 11:06:42 +0200

    Did eval, deriv, sturm-chain and others Have to fix doing (cdr (deriv ..)) everytime to get rid of the leading 0 Will try to implement that directly into the deriv function

--- modulo.scm
+++ modulo.scm
@@ -1,12 +1,13 @@
 #lang racket
 (define p%q (lambda(p q)
               (if (< (length p) (length q)) p 
-                  (let ([subls (sublist p q)]) ;Get the sublist
-                    (p%q_aux (substract_lists p subls) q ))))) ;Substract each coefficient of p by those of subls
+                  (let ([p (reverse p)] [q (reverse q)])
+                    (let ([subls (sublist p q)]) ;Get the sublist
+                      (p%q_aux (substract_lists p subls) q )))))) ;Substract each coefficient of p by those of subls
 
 
 (define p%q_aux (lambda(p q)
-              (if (< (length (cdr p)) (length q)) (cdr p)
+              (if (< (length (cdr p)) (length q)) (reverse (cdr p))
                   (let ([subls (sublist p q)]) ;Get the sublist
                     (p%q_aux (substract_lists (cdr p) subls) q ))))) ;Substract each coefficient of p by those of subls
 
@@ -33,9 +34,11 @@ this method returns the list where each element of ls is multiplied by (p1/q1)
                   (let ((div  (/ (car p) (car q)) ))
                     (multiply_list q div)))))
 
-(define p '(4 3 0 2 1))
+
+
+(define p '(1 2 0 3 4))
 'p p
-(define q '(1 1 2))
+(define q '(2 1 1))
 'q q
 
 '(p%q p q) (p%q p q)
--- sturm.scm
+++ sturm.scm
@@ -14,15 +14,76 @@
 ; if p and q are respectively the repsentation of P(x) and Q(x),
 ; (p%q p q) returns the representation of R(x), the remainer of the division
 ; of P(x) by Q(x).
-(define p%q
-  (lambda (p q) "TODO"))
+(define p%q (lambda(p q)
+              (if (< (length p) (length q)) p 
+                  (let ([p (reverse p)] [q (reverse q)])
+                    (let ([subls (sublist p q)]) ;Get the sublist
+                      (p%q_aux (substract_lists p subls) q )))))) ;Substract each coefficient of p by those of subls
 
+
+(define p%q_aux (lambda(p q)
+              (if (< (length (cdr p)) (length q)) (reverse (cdr p))
+                  (let ([subls (sublist p q)]) ;Get the sublist
+                    (p%q_aux (substract_lists (cdr p) subls) q ))))) ;Substract each coefficient of p by those of subls
+
+
+#| Multiplies each element of the list ls by [[n]]|#
+(define (multiply_list ls n)
+  (map (lambda (x) (* n x)) ls))
+
+#| Substracts the list p by the list subls, element by element. If the list subls is too
+short, the remainder is appended to to the result
+|#
+(define substract_lists (lambda (p subls)
+                          (if (null? subls) p
+                              (cons (- (car p) (car subls))
+                                    (substract_lists (cdr p) (cdr subls))))))
+
+
+#|
+If p1 and q1 are the first coefficient of the lists,
+this method returns the list where each element of ls is multiplied by (p1/q1)
+|#
+(define sublist (lambda (p q)
+                  (if (zero? (car p)) (sublist (cdr p) q) ;Can't divide by 0
+                  (let ((div  (/ (car p) (car q)) ))
+                    (multiply_list q div)))))
 
 ; if p is a polynome, (sturm-chain p) returns the Sturm chain of p by decreasing
 ; order of powers.
-(define sturm-chain
-  (lambda (p) "TODO"))
+(define sturm-chain (lambda (p)
+                      (cons p (cons (cdr (deriv 0 p)) (sturm-chain-aux p)))))
 
+(define sturm-chain-aux (lambda (p)
+                        (let ([dp (cdr (deriv 0 p))])
+                          (let ([p_i (p%q p dp)])
+                            (if (or (null? p_i)
+                                    (zero? (car p_i)))
+                                '()
+                                (cons p_i (sturm-chain-aux dp)))))))
+                          
+                             
+
+#| If [[n]] = 0 and p a polynome, (deriv n p) gives the derivative of p |#
+(define deriv (lambda (n p)
+                (if (null? p) '()
+                      (cons (* n (car p))
+                            (deriv (+ n 1) (cdr p))))))
+
+
+(define eval (lambda (x p)
+               (if (null? p) '()
+                   (cons (eval-aux x (car p) 0) (eval x (cdr p))))
+               ))
+
+(define eval-aux (lambda (x p n)
+                   (if (null? p) 0
+                       (+ (* (car p) (expt x n))
+                             (eval-aux x (cdr p) (+ n 1))
+                             ))))
+               
+          
+               
 
 ; if p is a polynome and both a and b are numbers such that a < b,
 ; (count-roots p a b) returns the number of roots of p
@@ -38,3 +99,9 @@
   (lambda (p a b eps) "POSSIBLY TODO"))
 
 
+
+(define pol '(1 2 3))
+'pol pol
+(define sturm (sturm-chain pol))
+'sturm sturm
+'eval (eval 5 sturm)
